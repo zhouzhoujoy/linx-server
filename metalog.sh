f49f553f18b45576265ba8357c9b120f4f1ec678
ZizzyDizzyMC <ZizzyDizzyMC@gmail.com>
2021-02-10 19:44:17 -0500

    Update metalog.sh

--- metalog.sh
+++ metalog.sh
@@ -1,10 +1,11 @@
-#!bin/bash
+#!/bin/bash
 # For setting up cron with a logging function for metadata only.
 
 # First argument is Linx Meta Source Directory.
 # Second argument is the Linx Meta Log Directory
 
 # Add to crontab with "crontab -e"
-# Paste "*/1 * * * * /path/to/metalog.sh /path/to/meta/source /path/to/meta/log
+# Paste "*/1 * * * * /path/to/metalog.sh /path/to/meta/source/ /path/to/meta-log"
 # Change it to source and log directory paths.
+# The slash after source/ is important. It means everyhitng *inside* the source directory.
 rsync -a ${1} ${2}
