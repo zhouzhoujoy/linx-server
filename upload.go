49161feb7a0ba9f73c77c3179ae58a5717110591
ZizzyDizzyMC <ZizzyDizzyMC@gmail.com>
2021-02-21 20:40:53 -0500

    Update upload.go

--- upload.go
+++ upload.go
@@ -228,7 +228,7 @@ func uploadRemote(c web.C, w http.ResponseWriter, r *http.Request) {
 }
 
 func uploadHeaderProcess(r *http.Request, upReq *UploadRequest) {
-	if len(r.Header.Get("Content-Length") > 0 {
+	if len(r.Header.Get("Content-Length")) > 0 {
 		if r.Header.Get("Content-Length") > Config.maxSize {
 			upReq.size = r.Header.Get("Content-Length")
 		}
